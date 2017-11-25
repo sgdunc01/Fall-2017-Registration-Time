fea63fea81d93472a114faf42940b3872fe2bbe7
Spencer Duncan <spencergrahamduncan@gmail.com>
2017-11-25 17:04:54 -0500

    Initial commit

---
+++ Prog2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Prog2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.creditHours = new System.Windows.Forms.TextBox();
+            this.registrationBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Your Last Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Credit Hours Earned (prior to Spring 2016):";
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(252, 22);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(100, 20);
+            this.lastName.TabIndex = 2;
+            // 
+            // creditHours
+            // 
+            this.creditHours.Location = new System.Drawing.Point(252, 58);
+            this.creditHours.Name = "creditHours";
+            this.creditHours.Size = new System.Drawing.Size(100, 20);
+            this.creditHours.TabIndex = 3;
+            // 
+            // registrationBtn
+            // 
+            this.registrationBtn.AutoSize = true;
+            this.registrationBtn.Location = new System.Drawing.Point(137, 128);
+            this.registrationBtn.Name = "registrationBtn";
+            this.registrationBtn.Size = new System.Drawing.Size(99, 23);
+            this.registrationBtn.TabIndex = 4;
+            this.registrationBtn.Text = "Registration Time";
+            this.registrationBtn.UseVisualStyleBackColor = true;
+            this.registrationBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 182);
+            this.Controls.Add(this.registrationBtn);
+            this.Controls.Add(this.creditHours);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fall Registration Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.TextBox creditHours;
+        private System.Windows.Forms.Button registrationBtn;
+    }
+}
+
---
+++ Prog2/Form1.cs
@@ -0,0 +1,223 @@
+﻿// Program 2
+// CIS 199-01/-75
+// Due: 3/06/2016
+// By: Spencer G. Duncan
+
+// This program displays a message box that informs
+// the user when they can register for classes in 
+// Fall 2016. The user must enter their last name 
+// and number of credit hours obtained.Does not use loops. 
+ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int SENIOR_HOURS = 90;//stores the minimum number of hours required to be a senior
+            const int JUNIOR_HOURS = 60;//stores the minimum number of hours required to be a junior
+            const int SOPHMORE_HOURS = 30;//stores the minimum number of hours required to be a sophmore
+
+            char name; //stores the lower case letter users last name
+            string hold;//stores the users last name
+            string day;//stores the day the user registers
+            string time;//stores the time the user registers
+            float hours;//stores the number of credit hours a student has aquired
+
+            hold = lastName.Text.ToLowerInvariant();
+            name = hold[0];
+            day = "";
+            time = "";
+
+            if (char.IsLetter(hold, 0))
+            {
+                if (float.TryParse(creditHours.Text, out hours) && (hours > 0))
+                {
+                    //Tests if you are a Senior and when you register
+                    if (hours >= SENIOR_HOURS)
+                    {
+                        day = "Wednesday, March 30th";
+                        if (name <= 'd')
+                        {
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 'i')
+                        {
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'o')
+                        {
+                            time = "10:00 AM";
+                        }
+                        else if (name <= 's')
+                        {
+                            time = "11:30 AM";
+                        }
+                        else
+                        {
+                            time = "2:00 PM";
+                        }
+                    }
+
+                    //Tests if you are a Junior and when you register
+                    else if (hours >= JUNIOR_HOURS)
+                    {
+                        day = "Thursday, March 31st";
+                        if (name <= 'd')
+                        {
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 'i')
+                        {
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'o')
+                        {
+                            time = "10:00 AM";
+                        }
+                        else if (name <= 's')
+                        {
+                            time = "11:30 AM";
+                        }
+                        else
+                        {
+                            time = "2:00 PM";
+                        }
+                    }
+
+                    //Tests if you are a Sophmore and when you register
+                    else if (hours >= SOPHMORE_HOURS)
+                    {
+                        if (name <= 'b')
+                        {
+                            day = "Monday, April 4th";
+                            time = "2:00 PM";
+                        }
+                        else if (name <= 'd')
+                        {
+                            day = "Monday, April 4th";
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 'f')
+                        {
+                            day = "Friday, Aril 1st";
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'i')
+                        {
+                            day = "Friday, April 1st";
+                            time = " 10:00 AM";
+                        }
+                        else if (name <= 'l')
+                        {
+                            day = "Friday, April 1st";
+                            time = "11:30 AM";
+                        }
+                        else if (name <= 'o')
+                        {
+                            day = "Friday, April 1st";
+                            time = "2:00 PM";
+                        }
+                        else if (name <= 'q')
+                        {
+                            day = "Friday, April 1st";
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 's')
+                        {
+                            day = "Monday, April 4th";
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'v')
+                        {
+                            day = "Monday, April 4th";
+                            time = "10:00 AM";
+                        }
+                        else
+                        {
+                            day = "Monday, April 4th";
+                            time = "11:30 AM";
+                        }
+                    }
+
+                    //Tests if you are a Freshman and when you register
+                    else
+                    {
+                        if (name <= 'b')
+                        {
+                            day = "Wednesday, April 6th";
+                            time = "2:00 PM";
+                        }
+                        else if (name <= 'd')
+                        {
+                            day = "Wednesday, April 6th";
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 'f')
+                        {
+                            day = "Tuesday, April 5th";
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'i')
+                        {
+                            day = "Tuesday, April 5th";
+                            time = " 10:00 AM";
+                        }
+                        else if (name <= 'l')
+                        {
+                            day = "Tuesday, April 5th";
+                            time = "11:30 AM";
+                        }
+                        else if (name <= 'o')
+                        {
+                            day = "Tuesday, April 5th";
+                            time = "2:00 PM";
+                        }
+                        else if (name <= 'q')
+                        {
+                            day = "Tuesday, April 5th";
+                            time = "4:00 PM";
+                        }
+                        else if (name <= 's')
+                        {
+                            day = "Wednesday, April 6th";
+                            time = "8:30 AM";
+                        }
+                        else if (name <= 'v')
+                        {
+                            day = "Wednesday, April 6th";
+                            time = "10:00 AM";
+                        }
+                        else
+                        {
+                            day = "Wednesday, April 6th";
+                            time = "11:30 AM";
+                        }
+                    }
+                    MessageBox.Show("The earliest you can register is " + day + " at " + time + ".");
+                }
+
+                else
+                    MessageBox.Show("Invalid Value Entered Into the Credit Hours Textbox.");
+            }
+            else
+                MessageBox.Show("Invalid Value Entered Into the Last Name Textbox.");
+        }
+    }
+}
+
